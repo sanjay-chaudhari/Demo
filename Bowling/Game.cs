e3b46157c2680470c50b4113d3b3fed308c712a7
Chaudhari, Sanjay <sanjay.chaudhari@ecolab.com>
2021-09-02 05:47:30 +0000

    Add project files.

---
+++ Bowling/Game.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+
+namespace Bowling
+{
+    public class Game
+    {
+        private int _scoreValue = 0;
+        private readonly List<int> _rolls = new List<int>();
+
+        #region Public Methods
+        //List maitaining Scores of all played rolls..
+        public void Roll(int pins)
+        {
+            _rolls.Add(pins);
+        }
+
+        public int GetScore()
+        {
+            int FrameIndex = 0;
+            for (int Frame = 0; Frame < 10; Frame++)
+            {
+                if (_rolls.Count - 2 == FrameIndex && _rolls[FrameIndex - 1] == 10 && _rolls[FrameIndex] != 10)
+                    break;
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex] == 10)
+                {
+                    _scoreValue = _scoreValue + _rolls[FrameIndex];
+                    Roll(10);
+                    _scoreValue += _rolls[_rolls.Count - 1];
+                    Roll(10);
+                    _scoreValue += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 2 == FrameIndex && _rolls[FrameIndex] == 10)
+                {
+                    Roll(10);
+                    Roll(10);
+                }
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex - 1] == 10)
+                {
+                    _scoreValue = _scoreValue + _rolls[FrameIndex];
+                    Roll(1);
+                    _scoreValue += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex % 2 != 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex - 1] == 10)
+                {
+                    _scoreValue += _scoreValue + _rolls[FrameIndex];
+                    Roll(1);
+                    _scoreValue += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex + 1 == _rolls.Count || FrameIndex == _rolls.Count)
+                    break;
+
+                //If strike then get next 2 roll's points as bonus...
+                if (IsStrike(FrameIndex))
+                {
+                    _scoreValue += StrikeScore(FrameIndex);
+                    FrameIndex++;
+                }
+                else
+                {
+                    //If Spare then get next frame's first roll's points as bonus...
+                    if (IsSpare(FrameIndex))
+                    {
+                        _scoreValue += SpareScore(FrameIndex);
+                    }
+                    else
+                    {
+                        _scoreValue += FrameScore(FrameIndex);
+                    }
+                    FrameIndex += 2;
+                }
+            }
+            return _scoreValue;
+        }
+        #endregion
+
+
+
+        #region Private Methods
+        //Check whether Strike...
+        private bool IsStrike(int frameIndex) => _rolls[frameIndex] == 10;
+
+        //Calculate Frame score...
+        private int FrameScore(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1];
+
+        //If strike then get next 2 roll's points as bonus...
+        private int StrikeScore(int frameIndex) => 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+
+        //If Spare then get next frame's first roll's points as bonus...
+        private int SpareScore(int frameIndex) => 10 + _rolls[frameIndex + 2];
+
+        //Check whether Spare...
+        private bool IsSpare(int frameIndex) => (_rolls[frameIndex] + _rolls[frameIndex + 1]) == 10;
+        #endregion
+    }
+}
---
+++ BowlingFixtures/GameFixture.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingFixtures
+{
+    [TestClass]
+    public class GameFixture
+    {
+        [TestMethod]
+        public void Validate_ZeroScore()
+        {
+            Bowling.Game g = new Bowling.Game();
+            for (int i = 0; i < 20; i++)
+                g.Roll(0);
+            Assert.AreEqual(0, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_CorrectSpareScore()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
+            Assert.AreEqual(13, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_CorrectStrikeScore()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(10);
+            g.Roll(5);
+            g.Roll(6);
+            Assert.AreEqual(21, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_AllStrikes()
+        {
+            Bowling.Game g = new Bowling.Game();
+            for (int i = 0; i < 10; i++)
+            {
+                g.Roll(10);
+            }
+            Assert.AreEqual(300, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_AllOnes()
+        {
+            Bowling.Game g = new Bowling.Game();
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+            Assert.AreEqual(20, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_AllCommonRecords()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(3);
+            g.Roll(3);
+            g.Roll(3);
+            g.Roll(3);
+            g.Roll(4);
+            g.Roll(4);
+            g.Roll(4);
+            g.Roll(4);
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(6);
+            g.Roll(6);
+            g.Roll(6);
+            g.Roll(6);
+            g.Roll(7);
+            g.Roll(7);
+            g.Roll(7);
+            g.Roll(7);
+            Assert.AreEqual(111, g.GetScore());
+        }
+
+
+        [TestMethod]
+        public void Check_NormalsCount()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(1);
+            g.Roll(4);
+
+            g.Roll(4);
+            g.Roll(5);
+
+            g.Roll(6);
+            g.Roll(4);
+
+            g.Roll(5);
+            g.Roll(5);
+
+            g.Roll(10);
+
+            g.Roll(0);
+            g.Roll(1);
+
+            g.Roll(7);
+            g.Roll(3);
+
+            g.Roll(6);
+            g.Roll(4);
+
+            g.Roll(10);
+
+            g.Roll(2);
+            g.Roll(8);
+            g.Roll(6);
+
+            Assert.AreEqual(133, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_ActualScenarioForAssignment()
+        {
+            Bowling.Game g = new Bowling.Game();
+            // 1 Set
+            g.Roll(10);
+
+            // 2 Set
+            g.Roll(9);
+            g.Roll(1);
+
+            // 3 Set
+            g.Roll(5);
+            g.Roll(5);
+
+            // 4 Set
+            g.Roll(7);
+            g.Roll(2);
+
+            // 5 Set
+            g.Roll(10);
+
+            // 6 Set
+            g.Roll(10);
+
+            // 7 Set
+            g.Roll(10);
+
+            // 8 Set
+            g.Roll(9);
+            g.Roll(0);
+
+            // 9 Set
+            g.Roll(8);
+            g.Roll(2);
+
+            // 10 Set
+            g.Roll(9);
+            g.Roll(1);
+            g.Roll(10);
+
+            Assert.AreEqual(187, g.GetScore());
+        }
+
+
+        [TestMethod]
+        public void Check_RandomGameNoExtraRoll()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(1);
+            g.Roll(3);
+            g.Roll(7);
+            g.Roll(3);
+            g.Roll(10);
+            g.Roll(1);
+            g.Roll(7);
+            g.Roll(5);
+            g.Roll(2);
+            g.Roll(5);
+            g.Roll(3);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(10);
+            g.Roll(9);
+            g.Roll(0);
+            Assert.AreEqual(122, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_RandomGameWithSpareThenStrikeAtEnd()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(1);
+            g.Roll(3);
+            g.Roll(7);
+            g.Roll(3);
+            g.Roll(10);
+            g.Roll(1);
+            g.Roll(7);
+            g.Roll(5);  
+            g.Roll(2);
+            g.Roll(5);
+            g.Roll(3);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(10);
+            g.Roll(9);
+            g.Roll(1);
+            g.Roll(10);
+            Assert.AreEqual(143, g.GetScore());
+        }
+
+        [TestMethod]
+        public void Check_RandomGameWithThreeStrikesAtEnd()
+        {
+            Bowling.Game g = new Bowling.Game();
+            g.Roll(1);
+            g.Roll(3);
+            g.Roll(7);
+            g.Roll(3);
+            g.Roll(10);
+            g.Roll(1);
+            g.Roll(7);
+            g.Roll(5);
+            g.Roll(2);
+            g.Roll(5);
+            g.Roll(3);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
+            Assert.AreEqual(163, g.GetScore());
+        }
+    }
+}
